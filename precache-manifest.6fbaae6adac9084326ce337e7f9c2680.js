8391f28389c219dc2ed18764ccc4ef36fe41638d
Sher Ali <sherali2910.sa@gmail.com>
2019-12-12 16:50:20 +0530

    Updates

---
+++ precache-manifest.6fbaae6adac9084326ce337e7f9c2680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791288c9967cfe902bd1d114371be80c",
+    "url": "/react-redux-todo-app/index.html"
+  },
+  {
+    "revision": "ff534611dca31d7de628",
+    "url": "/react-redux-todo-app/static/css/main.dd98955e.chunk.css"
+  },
+  {
+    "revision": "68d5fd8f1cbd8fe60abc",
+    "url": "/react-redux-todo-app/static/js/2.06fe1e60.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-redux-todo-app/static/js/2.06fe1e60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff534611dca31d7de628",
+    "url": "/react-redux-todo-app/static/js/main.db41e2e0.chunk.js"
+  },
+  {
+    "revision": "23192c6923a271149f0a",
+    "url": "/react-redux-todo-app/static/js/runtime-main.c21db773.js"
+  }
+]);
